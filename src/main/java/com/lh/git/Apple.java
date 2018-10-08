99e5075b5ff6208d8a45217fd7bb947736efad83
eclipse_user <eclipse_user@163.com>
2018-10-08 21:42:47 +0800

    my first commit in eclipse

---
+++ src/main/java/com/lh/git/Apple.java
@@ -0,0 +1,16 @@
+package com.lh.git;
+
+/** 
+* @ClassName: Apple 
+* @Description: TODO
+* @author Liu 
+* @date 2018年10月8日 下午9:02:53 
+*/
+public class Apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/lh/git/AppleTest.java
@@ -0,0 +1,11 @@
+package com.lh.git;
+
+/** 
+* @ClassName: AppleTest 
+* @Description: TODO
+* @author Liu 
+* @date 2018年10月8日 下午9:04:48 
+*/
+public class AppleTest {
+
+}
